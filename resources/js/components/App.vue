5f0180da2694e88f5af0673f03b41830e43acc18
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-06-25 20:51:22 +0600

    1 commit contains lara & vue blade together

---
+++ resources/js/components/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <h2>vue 3 component</h2>
+    <p>Sohan Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum dignissimos cum blanditiis qui. Dignissimos, repudiandae enim, neque maiores tenetur reprehenderit possimus, nobis dolores dolorem iusto nostrum ab odit animi quia.</p>
+</template>
+<script>
+    export default {
+
+    }
+</script>
+<style>
+
+</style>
